b69eab0948b9d208dd95ccefedbd712cf3a4e7fa
Anatolii Shumov <seminarsds@gmail.com>
2023-12-15 17:15:04 +0100

    first commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Anatolii Shumov on 15/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .red
+    }
+
+
+}
+
